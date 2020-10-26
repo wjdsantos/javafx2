5bc14ebe4e002f23ba7d799a2faa3284a8271aa7
Walter dos Santos <walter4asantos@gmail.com>
2020-10-26 12:46:25 -0300

    Mostrando Alert

--- src/gui/ViewController.java
+++ src/gui/ViewController.java
@@ -1,6 +1,8 @@
 package gui;
 
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 
 public class ViewController {
@@ -10,6 +12,12 @@ public class ViewController {
 	
 	@FXML
 	public void onBtTestAction() {
-		System.out.println("Click");
+		//Alerts.showAlert("Alert title", "Alert header", "Hello", AlertType.CONFIRMATION); //Aparece um ícone com 
+															// a letra 'I' de Information e os boões 'OK' e 'CANCEL'
+		
+		//Alerts.showAlert("Alert title", "Alert header", "Hello", AlertType.ERROR); //Aparece um ícone vermelho com 
+															//a letra 'X' e o botão 'OK'
+		
+		Alerts.showAlert("Alert title", null, "Hello", AlertType.ERROR); //Fica mais enxuto pois não tem o Header
 	}
 }
